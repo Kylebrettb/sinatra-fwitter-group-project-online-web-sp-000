96c9daf8e4d98f709b5bdef731b28ea2b1f76b0f
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-10-10 20:12:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 require './config/environment'
 
+require './app/helpers/helpers.rb'
 class ApplicationController < Sinatra::Base
+
+  extend Helpers
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
 end
+end 
